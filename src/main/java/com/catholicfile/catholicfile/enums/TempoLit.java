a7da1560872508644100e66d694cf907a71dda32
raissaadev <raissadematossouza@gmail.com>
2026-05-01 22:13:58 -0300

    CORREÇÃO: IMPORT DA CLASSE CORRETA

--- src/main/java/com/catholicfile/catholicfile/enums/TempoLit.java
+++
@@ -1,9 +0,0 @@
-package com.catholicfile.catholicfile.enums;
-
-public enum TempoLit {
-    ADVENTO,
-    NATAL,
-    QUARESMA,
-    PASCOA,
-    COMUM
-}
--- src/main/java/com/catholicfile/catholicfile/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.catholicfile.catholicfile.enums;
-
-public enum UserRole {
-    USUARIO,
-    ADMINISTRADOR
-}
